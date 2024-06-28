77507f4134348b7e860e4529bf4bfd1de4d483de
RAJPUT SHUBHAM <145737306+subahm6@users.noreply.github.com>
2024-06-28 10:11:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,10 +71,7 @@ def service_page():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",debug=True,port=80)
+    app.run(debug=True)
 
     
 
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1",debug=True,port=80)
