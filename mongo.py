a32f93b6e88721f80c5e9a6a8ae210d74b520b27
RAJPUT SHUBHAM <145737306+subahm6@users.noreply.github.com>
2024-06-28 10:06:25 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,22 +1,22 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
+#import psycopg2
 
-mongodb_client = MongoClient(host="localhost:27017", connect=True)
-database = mongodb_client["admin"]
-collection = database.get_collection("user_credentials")
+#mongodb_client = MongoClient(host="localhost:27017", connect=True)
+#database = mongodb_client["admin"]
+#collection = database.get_collection("user_credentials")
 
 
 
-def startup_db_client(collection,document):
+#def startup_db_client(collection,document):
+ #   
+  #  result = collection.insert_one(document)
+   # print(f"Document inserted with ID: {result.inserted_id}")
+
+#def get_data(collection,user,passw):
+ #   docs = collection.find()
+  #  for i in docs:
+   #     if i['user'] == user and i['pass'] == passw:
+    #        return True
+     #   else:
+      #      return False
     
-    result = collection.insert_one(document)
-    print(f"Document inserted with ID: {result.inserted_id}")
-
-def get_data(collection,user,passw):
-    docs = collection.find()
-    for i in docs:
-        if i['user'] == user and i['pass'] == passw:
-            return True
-        else:
-            return False
-    
-print(get_data(collection,'singhfuleshwar552@gmail.com', 'subhamsinghhkiller'))
